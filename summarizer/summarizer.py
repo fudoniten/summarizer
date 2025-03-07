a654a1710b64c1312120c92beb85ec30db1c661e
niten <niten@fudo.org>
2025-03-07 09:24:53 -0800

    Allow instructions to be stored in files.

--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -37,23 +37,33 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('filename', type=str, help='text document to be summarized.')
 parser.add_argument('-i', '--initial-instructions', type=str,
-                     help="instructions to ollama on how the document shoud initially be summarized (during the init/map step).",
-                     required=True)
+                     help="instructions to ollama on how the document should initially be summarized (during the init/map step).")
+parser.add_argument('-I', '--initial-instructions-file', type=str,
+                    help="file containing instructions on how the document should initially be summarized (during the init/map step).")
 parser.add_argument('-s', '--merge-instructions', type=str,
                     help="instructions on how the enhance or integration step should combine documents (during the refine/reduce step).")
+parser.add_argument('-S', '--merge-instructions-file', type=str,
+                    help="file containing instructions on how the enhance or integration step should combine documents (during the refine/reduce step).")
 parser.add_argument('-m', '--model', type=str, help='model to use by ollama.')
-parser.add_argument('-S', '--chunk_size', type=int,
+parser.add_argument('-S', '--chunk-size', type=int,
                      help='size of chunks to break text into for summary.', default=1900)
 parser.add_argument('-o', '--overlap', type=int,
                      help='size of overlap between chunks, to provide shared context.',
                      default=256)
-parser.add_argument('-t', '--summary_type',
+parser.add_argument('-t', '--summary-type',
                      choices=['map_reduce', 'refine'],
                      help='what approach to use when summarizing: map_reduce (parallel) or refine (sequential).',
                      default='map_reduce')
 parser.add_argument('-u', '--server', type=str,
                      help='URL of the ollama server.')
 parser.add_argument('-v', '--verbose', action='store_true', help='provide verbose output.')
+
+
+def read_file_contents(filename):
+    content = None
+    with open(filename) as f:
+        content = f.read()
+    return content
 
 
 def main():
@@ -84,10 +94,20 @@ def main():
       sys.exit(2)
     ollama_model = os.getenv('OLLAMA_MODEL')
 
-  init_step = args.initial_instructions
+  init_step = None
+  if args.initial_instructions is not None:
+    init_step = args.initial_instructions
+  elif args.initial_instructions_file is not None:
+    init_step = read_file_contents(args.initial_instructions_file)
+  else:
+    print("error: no instructions provided, use -i or -I")
+    sys.exit(2)
+
   merge_step = None
   if args.merge_instructions is not None:
     merge_step = args.merge_instructions
+  elif args.merge_instructions_file is not None:
+    merge_step = read_file_contents(args.merge_instructions_file)
   else:
     merge_step = init_step
     
